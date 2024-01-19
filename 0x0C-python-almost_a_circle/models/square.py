55bb296deba4e5ba62d29fb440726e6ef50ae57f
Asmae2001 <himdiasmaebr@gmail.com>
2024-01-19 06:46:14 -0800

    task

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class, inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor method for Square class"""
+
+        # Call the constructor of the parent class (Rectangle)
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter method for size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Override the __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
