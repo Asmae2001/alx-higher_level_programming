4372aaf89f0235e7869ce7b2aa463d4df7243662
Asmae2001 <himdiasmaebr@gmail.com>
2024-01-19 06:52:44 -0800

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,23 +23,14 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """Assign attributes using *args and **kwargs"""
-
-        if args:
-            # Assign attributes based on the order in *args
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        elif kwargs:
-            # Assign attributes based on **kwargs
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    def to_dictionary(self):
+        """Return dictionary representation of the Square"""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
 
     def __str__(self):
         """Override the __str__ method"""
