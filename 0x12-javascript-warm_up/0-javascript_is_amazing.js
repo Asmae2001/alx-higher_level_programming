d0c140bc455bf893eaf6b2bafc347724db61ea5d
Asmae2001 <himdiasmaebr@gmail.com>
2024-03-11 15:19:02 -0700

    task

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-let myVar = 'Javascript is amazing';
-// function to print myVar
+// Print string “Javascript is amazing”
+const myVar = 'Javascript is amazing';
 console.log(myVar);
